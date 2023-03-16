3633f59c968e089e421b0a0234ab4186be158298
pawan <pawanpk9035@gmail.com>
2023-03-16 16:17:22 +0530

    file to create ec2 instance

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
-resource "aws_instance" "my-ec2-instance" {
+resource "aws_instance" "my-instance" {
  
  ami = "ami-0f8ca728008ff5af4"
  instance_type = "t2.micro"
+ count = 1
  tags = {
      Name = "TERRAFORM"
  }
